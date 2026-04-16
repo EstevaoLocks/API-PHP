682c6aa5468aa96c88cdcb6eedf45731bcf4d151
estevaolocks <estevao.locks11@gmail.com>
2026-04-16 09:14:08 -0300

    sincronizando e alterando retorno do metodo get

--- api_php/api.php
+++ api_php/api.php
@@ -5,9 +5,19 @@
 
     // echo "Método da Requisição: " . $metodo;
 
+    // CONTEÚDO
+    $usuarios = [
+        ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
+        ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
+    ];
+
+
     switch ($metodo) {
         case 'GET':
-            echo "AQUI AÇÕES DO MÉTODO GET";
+            // echo "AQUI AÇÕES DO MÉTODO GET";
+
+            // Converte para Json e retorna
+            echo json_encode($usuarios);
             break;
         case 'POST':
             echo "AQUI AÇÕES DO MÉTODO POST";
@@ -17,10 +27,6 @@
             break;
     }
 
-    // $usuarios = [
-    //     ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
-    //     ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
-    // ];
 
     // echo json_encode($usuarios);
 
