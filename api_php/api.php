a5a1d269cdcf109cfb991254ba34782639482aac
estevaolocks <estevao.locks11@gmail.com>
2026-04-23 10:11:26 -0300

    sincronizar

--- api_php/api.php
+++ api_php/api.php
@@ -6,10 +6,17 @@
     // echo "Método da Requisição: " . $metodo;
 
     // CONTEÚDO
-    $usuarios = [
-        ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
-        ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
-    ];
+    // $usuarios = [
+    //     ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
+    //     ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
+    // ];
+    
+    $arquivo = 'usuarios.json';
+
+    if (!file_exists($arquivo)) {
+        file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+    $usuarios = json_decode(file_get_contents($arquivo), true);
 
 
     switch ($metodo) {
@@ -19,35 +26,46 @@
             // Converte para Json e retorna
             echo json_encode($usuarios);
             break;
+
         case 'POST':
             // echo "AQUI AÇÕES DO MÉTODO POST";
 
             $dados = json_decode(file_get_contents('php://input'), true);
             // print_r($dados);
 
-            // Convertendo dados json para php
-            // $novoUsuario = [
-            //     "id" => $dados["id"],
-            //     "nome" => $dados["nome"],
-            //     "email" => $dados["email"]
-            // ];
-
-            $arquivo = 'usuarios.json';
-
-            if (!file_exists($arquivo)) {
-                file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+            if (!isset($dados["id"]) || !isset($dados["nome"]) || !isset($dados["email"])){
+                http_response_code(400);
+                echo json_encode(["erro" => "Dados incompletos"], JSON_UNESCAPED_UNICODE);
+                exit;
             }
 
-            $usuarios = json_decode(file_get_contents($arquivo), true);
+            // Convertendo dados json para php
+            $novoUsuario = [
+                "id" => $dados["id"],
+                "nome" => $dados["nome"],
+                "email" => $dados["email"]
+            ];
+
+            // Adiciona usuários ao Array
+            $usuarios[] = $novoUsuario;
+
+            // Salva Array no arquivo Json
+            file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+            // Mensagem de sucesso
+            echo json_encode(["mensagem" => "Usuário inserido com sucesso!", "usuarios" =>  $usuarios], JSON_UNESCAPED_UNICODE);
+
 
             // Adiciona um novo usuário a um usuário existente
-            array_push($usuarios, $novoUsuario);
-            echo json_encode('Usuário inserido com sucesso!');
-            print_r($usuarios);
+            // array_push($usuarios, $novoUsuario);
+            // echo json_encode('Usuário inserido com sucesso!');
+            // print_r($usuarios);
 
             break;
+
         default:
-            echo "MÉTODO NÃO ENCONTRADO!";
+            // echo "MÉTODO NÃO ENCONTRADO!";
+            http_response_code(405); // Método não permitido
             break;
     }
 
